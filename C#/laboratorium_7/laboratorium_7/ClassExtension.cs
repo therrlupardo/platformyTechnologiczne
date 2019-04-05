589ee58763376ea15859f6e2708d49e4fe23ad78
Mateusz Buchajewicz <theherrlupardo@gmail.com>
2019-04-06 01:47:23 +0200

    lab7 pt added

---
+++ C#/laboratorium_7/laboratorium_7/ClassExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace laboratorium_7
+{
+    public static class ClassExtension
+    {
+        public static void PrintThisNicely(this DirectoryInfo aSampleDir)
+        {
+            Console.WriteLine($"This is a directory named: ${aSampleDir.FullName}");
+        }
+    }
+
+
+    public class ASampleClass
+    {
+        public void SampleMethod()
+        {
+            var dir = new DirectoryInfo("C:/Users/");
+            dir.PrintThisNicely();
+        }
+    }
+}
