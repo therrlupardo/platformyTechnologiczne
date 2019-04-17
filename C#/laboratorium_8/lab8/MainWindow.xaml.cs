b51b2b5493a23fcec679b2679773d11e2dfaf540
mbuchajewicz <theherrlupardo@gmail.com>
2019-04-17 14:23:16 +0200

    context menu - delete directory

--- C#/laboratorium_8/lab8/MainWindow.xaml.cs
+++ C#/laboratorium_8/lab8/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MenuItem = System.Windows.Controls.MenuItem;
 
 namespace lab8
 {
@@ -44,38 +45,104 @@ namespace lab8
             }
         }
 
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
         private TreeViewItem MakeTreeFile(FileInfo file)
         {
             var item = new TreeViewItem
             {
                 Header = file.Name,
-                Tag = file.Name
+                Tag = file.FullName
             };
+            item.ContextMenu = new System.Windows.Controls.ContextMenu();
+            var menuItem1 = new MenuItem { Header = "Open" };
+            menuItem1.Click += new RoutedEventHandler(MenuItemOpenClick);
+            var menuItem2 = new MenuItem { Header = "Delete" };
+            menuItem2.Click += new RoutedEventHandler(MenuItemDeleteClick);
+            item.ContextMenu.Items.Add(menuItem1);
+            item.ContextMenu.Items.Add(menuItem2);
             return item;
         }
+
+       
 
         private TreeViewItem MakeTreeDirectory(DirectoryInfo dir)
         {
             var root = new TreeViewItem
             {
                 Header = dir.Name,
-                Tag = dir.Name
+                Tag = dir.FullName
             };
+
+            root.ContextMenu = new System.Windows.Controls.ContextMenu();
+            var menuItem1 = new MenuItem { Header = "Create" };
+            menuItem1.Click += new RoutedEventHandler(MenuItemCreateClick);
+            var menuItem2 = new MenuItem { Header = "Delete" };
+            menuItem2.Click += new RoutedEventHandler(MenuItemDeleteClick);
+            root.ContextMenu.Items.Add(menuItem1);
+            root.ContextMenu.Items.Add(menuItem2);
 
             foreach (DirectoryInfo subdir in dir.GetDirectories())
             {
-                root.Items.Add(this.MakeTreeDirectory(subdir));
+                root.Items.Add(MakeTreeDirectory(subdir));
             }
             foreach (FileInfo file in dir.GetFiles())
             {
-                root.Items.Add(this.MakeTreeFile(file));
+                root.Items.Add(MakeTreeFile(file));
             }
             return root;
         }
 
-            private void Exit(object sender, RoutedEventArgs e)
+        private void MenuItemCreateClick(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            throw new NotImplementedException();
+        }
+
+        private void MenuItemDeleteClick(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
+            String path = (String)item.Tag;
+            FileAttributes attributes = File.GetAttributes(path);
+            File.SetAttributes(path, attributes & ~FileAttributes.ReadOnly);
+            if((attributes & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                deleteDirectory(path);
+            }
+            else
+            {
+                File.Delete(path);
+            }
+            if ((TreeViewItem) treeView.Items[0] != item)
+            {
+                TreeViewItem parent = (TreeViewItem)item.Parent; 
+                parent.Items.Remove(item);
+            }
+            else
+            {
+                treeView.Items.Clear();
+            }
+        }
+
+        private void deleteDirectory(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            foreach(var subdir in dir.GetDirectories())
+            {
+                deleteDirectory(subdir.FullName);
+            }
+            foreach(var file in dir.GetFiles())
+            {
+                File.Delete(file.FullName);
+            }
+            Directory.Delete(path);
+        }
+
+        private void MenuItemOpenClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
