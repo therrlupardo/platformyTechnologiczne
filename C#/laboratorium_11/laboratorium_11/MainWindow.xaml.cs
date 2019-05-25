6ed625ca2865564489b0ac304cd0d6493fbcf37c
Mateusz Buchajewicz <theherrlupardo@gmail.com>
2019-05-25 19:07:54 +0200

    pt lab11 1+2 + window

---
+++ C#/laboratorium_11/laboratorium_11/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laboratorium_11
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private NewtonSymbol newtonSymbol;
+        private int highestPercentageReached;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ButtonClick_NetwonSymbolTasks(object sender, RoutedEventArgs e)
+        {
+            int k, n;
+            if (!Int32.TryParse(textBox_n.Text, out n) || !Int32.TryParse(textBox_k.Text, out k))
+            {
+                SetErrorLabel("Aby obliczyć wartość Symbolu Newtona musisz najpierw ustawić n i k!");
+                return;
+            }
+            newtonSymbol = new NewtonSymbol(n, k);
+            double result = newtonSymbol.CalculateTasks();
+            switch (result)
+            {
+                case -1:
+                    SetErrorLabel("n i k muszą być większe od 0!");
+                    break;
+                case -2:
+                    SetErrorLabel("k nie może być większe niż n!");
+                    break;
+                default:
+                    textBox_tasks.Text = result.ToString();
+                    SetErrorLabel("");
+                    break;
+            }
+            
+        }
+        private void ButtonClick_NetwonSymbolDelegates(object sender, RoutedEventArgs e)
+        {
+            int k, n;
+            if (!Int32.TryParse(textBox_n.Text, out n) || !Int32.TryParse(textBox_k.Text, out k))
+            {
+                SetErrorLabel("Aby obliczyć wartość Symbolu Newtona musisz najpierw ustawić n i k!");
+                return;
+            }
+            newtonSymbol = new NewtonSymbol(n, k);
+            double result = newtonSymbol.CalculateDelegates();
+            switch (result)
+            {
+                case -1:
+                    SetErrorLabel("n i k muszą być większe od 0!");
+                    break;
+                case -2:
+                    SetErrorLabel("k nie może być większe niż n!");
+                    break;
+                default:
+                    textBox_delegates.Text = result.ToString();
+                    SetErrorLabel("");
+                    break;
+            }
+        }
+        private async void ButtonClick_NetwonSymbolAsyncAwait(object sender, RoutedEventArgs e)
+        {
+            int k, n;
+            if (!Int32.TryParse(textBox_n.Text, out n) || !Int32.TryParse(textBox_k.Text, out k))
+            {
+                SetErrorLabel("Aby obliczyć wartość Symbolu Newtona musisz najpierw ustawić n i k!");
+                return;
+            }
+            newtonSymbol = new NewtonSymbol(n, k);
+            double result = await newtonSymbol.CalculateAsyncAwait();
+            switch (result)
+            {
+                case -1:
+                    SetErrorLabel("n i k muszą być większe od 0!");
+                    break;
+                case -2:
+                    SetErrorLabel("k nie może być większe niż n!");
+                    break;
+                default:
+                    textBox_async_await.Text = result.ToString();
+                    SetErrorLabel("");
+                    break;
+            }
+
+        }
+        private void ButtonClick_Get(object sender, RoutedEventArgs e)
+        {
+            int i;
+            if(!Int32.TryParse(textBox_i.Text, out i))
+            {
+                SetErrorLabel("Aby obliczyć i-ty element ciągu fibonacciego musisz podać i!");
+            }
+            BackgroundWorker fibonacciWorker = new BackgroundWorker();
+            fibonacciWorker.DoWork += new DoWorkEventHandler(fibonacciWorker_DoWork);
+            fibonacciWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(fibonacciWorker_RunWorkerCompleted);
+            fibonacciWorker.ProgressChanged += new ProgressChangedEventHandler(fibonacciWorker_ProgressChanged);
+            highestPercentageReached = 0;
+            progressBar.Value = 0;
+            fibonacciWorker.RunWorkerAsync(i);
+        }
+
+        private void fibonacciWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            worker.WorkerReportsProgress = true;
+            e.Result = ComputeFibonacci((int)e.Argument, worker, e);
+        }
+
+        private void fibonacciWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            textBox_fibonacci.Text = e.Result.ToString();
+        }
+
+        private void fibonacciWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private UInt64 ComputeFibonacci(int n, BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            if(n < 0)
+            {
+                SetErrorLabel("Ciąg Fibonacciego można policzyć tylko dla i > 0!");
+                return 0;
+            }
+            UInt64 result = 0;
+
+            if (worker.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                List<UInt64> listOfFibonacciElements = new List<UInt64>();
+                
+                for(int i = 1; i <= n; i++)
+                {
+                    if (i <= 2)
+                    {
+                        listOfFibonacciElements.Add(1);
+                    }
+                    else
+                    {
+                        var a = listOfFibonacciElements.Last();
+                        listOfFibonacciElements.Remove(a);
+                        var b = listOfFibonacciElements.Last();
+                        listOfFibonacciElements.Add(a);
+                        listOfFibonacciElements.Add(a + b);
+                    }
+                    int percentComplete = (int)((float)i / n * 100);
+                    if (percentComplete > highestPercentageReached)
+                    {
+                        highestPercentageReached = percentComplete;
+                        worker.ReportProgress(percentComplete);
+                        Thread.Sleep(20);
+                    }
+                }
+                result = listOfFibonacciElements.Last();
+            }
+            
+
+            return result;
+        }
+
+       
+
+        private void ButtonClick_Resolve(object sender, RoutedEventArgs e) { }
+        private void ButtonClick_Compress(object sender, RoutedEventArgs e) { }
+        private void ButtonClick_Decompress(object sender, RoutedEventArgs e) { }
+        private void ButtonClick_Check(object sender, RoutedEventArgs e) { }
+        private void LabelDoubleClick_ClearErrorLabel(object sender, RoutedEventArgs e)
+        {
+            SetErrorLabel("");
+        }
+
+        private void SetErrorLabel(string error)
+        {
+            label_error.Content = error;
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
---
+++ C#/laboratorium_11/laboratorium_11/NewtonSymbol.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorium_11
+{
+    class NewtonSymbol
+    {
+        public int k { get; set; }
+        public int n { get; set; }
+
+
+        public NewtonSymbol(int n, int k)
+        {
+            this.n = n;
+            this.k = k;
+        }
+
+        public double CalculateTasks()
+        {
+            if (n <= 0 || k <= 0) return -1;
+            if (n < k) return -2;
+
+            Task<double> counterTask = Task.Factory.StartNew(
+                (obj) => CalculateCounter(),
+                100 //state
+                );
+
+            Task<double> denominatorTask = Task.Factory.StartNew(
+                (obj) => CalculateDenominator(),
+                100 //state
+                );
+
+            counterTask.Wait();
+            denominatorTask.Wait();
+            return counterTask.Result / denominatorTask.Result;
+        }
+
+        public double CalculateDelegates()
+        {
+            if (n <= 0 || k <= 0) return -1;
+            if (n < k) return -2;
+
+            Func<double> counterFunc = CalculateCounter;
+            Func<double> denominatorFunc = CalculateDenominator;
+
+            IAsyncResult counter, denominator;
+            counter = counterFunc.BeginInvoke(null, null);
+            denominator = denominatorFunc.BeginInvoke(null, null);
+            while (!counter.IsCompleted && !denominator.IsCompleted) ;
+
+            return counterFunc.EndInvoke(counter) / denominatorFunc.EndInvoke(denominator);
+        }
+
+
+        public async Task<double> CalculateAsyncAwait()
+        {
+            double counter = await Task.Run(() => CalculateCounter());
+            double denominator = await Task.Run(() => CalculateDenominator());
+
+            return counter / denominator;
+        }
+
+
+
+        private double CalculateCounter()
+        {
+            double returnValue = 1;
+            for (int i = (n - k + 1); i <= n; i++)
+            {
+                returnValue *= i;
+            }
+            return returnValue;
+        }
+        private double CalculateDenominator()
+        {
+            double returnValue = 1;
+            for (int i = 1; i <= k; i++)
+            {
+                returnValue *= i;
+            }
+            return returnValue;
+        }
+
+    }
+}
